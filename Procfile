e588f59d62b17802a1b902563d2e76e860e9aa39
izaxson <67207370+Izaxson@users.noreply.github.com>
2023-07-15 23:44:25 +0300

    fufua

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'realtrips.wsgi'
---
+++ accounts/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login ,logout
+from django.contrib import messages
+from django.views import View
+
+class LoginView(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect('dashboard')
+        else:
+            # Return an 'invalid login' error message.
+            messages.error(request, "An error occurred.")
+            return redirect('login')
+        
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.views import View
+
+class LogoutView(View):
+    def get(self, request):
+        logout(request)
+        # Redirect to a success page.
+        return redirect('login')
+
+       
